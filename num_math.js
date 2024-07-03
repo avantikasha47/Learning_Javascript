cc625c31c332a1e730a9d42e1626b5341eae8ca7
Avantika <avani007sharma@gmail.com>
2024-07-04 01:04:12 +0530

    done

---
+++ num_math.js
@@ -0,0 +1,40 @@
+const score=400
+// console.log( typeof score)
+const balance=new Number( 400)   //explicitly define type  in object
+// console.log( typeof balance)
+
+
+
+// console.log(balance.toString().length)     //change it as per i want 
+
+// console.log(balance.toFixed(2))     //how much point want after point
+const othernumber=123.896666666
+// console.log(othernumber.toPrecision(3))  //start is 3 pr focus kro
+
+
+
+const hundreds=10000000000
+// console.log(hundreds.toLocaleString("en-IN"))   //as per indian 
+
+//can check min_value max etc
+
+
+//..........................MATH library give function ..................................................
+
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.9))
+
+// console.log(Math.ceil(4.3))  //choose top
+// console.log(Math.floor(3.4))  //choose lower value
+
+// console.log(Math.min(4,9,2,3,4,5))
+
+// console.log(Math.random())   //value between 0 and 1
+
+// console.log((Math.random()*10)+1)
+
+ const min=10
+ const max=20
+console.log( Math.floor(Math.random()*(max-min+1))+min)   //give in fixed random values 
+
+
---
+++ string.js
@@ -0,0 +1,47 @@
+const name="avantika"
+const repo=50
+
+// console.log(name+ repo+" sharma")  //outdated syntax
+
+
+
+// console.log(`helloo my name is ${name.toUpperCase()}  and my count is ${repo}`) //better way with include function to string 
+
+const gamename=new String("avantika-sharma-sharma")
+// console.log(gamename[0])       //a
+// console.log(gamename.__proto__)   //{}
+// console.log(gamename.length)
+
+// console.log(gamename.toUpperCase())    to upper it
+// console.log(gamename.charAt(2))    is index per konsa h character
+
+// console.log(gamename.indexOf("t"))     get index
+
+// const newstring=gamename.substring(-1,-4)     //give start and end value   //not obey negative value 
+// console.log(newstring)
+
+
+
+// const anotherstring=gamename.slice(-8,-2)     //negative and postive treat from start
+// console.log(anotherstring)
+
+
+const newstringone="      avantika     "
+// console.log(newstringone)
+// console.log(newstringone.trim())     //remove spaces
+
+//trim start and trim end exist and so on 
+
+
+
+const url="https//avani.com%20sharma"
+
+// console.log(url.replace("%20","-"))       wanaaa replace with one with other 
+console.log(url.includes("avani"))        //puch skte h 
+
+console.log(gamename.split("-"))       //get array 
+
+
+
+
+//follow mdn or console ..................
